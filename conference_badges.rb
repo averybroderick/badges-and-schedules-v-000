dbefbc6fe01809ec6d623ced2a2a7005b383dd2b
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-22 14:54:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  badge_arr = []
+  arr.each do |name|
+    badge_arr.push(badge_maker(name))
+  end
+  return badge_arr
+end
